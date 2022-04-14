9a71d9ebdd8185b535071d6af705afe5627e0a15
EgorBodrov <bodroff.alexeevich@yandex.ru>
2022-04-14 22:51:36 +0300

    Change const values to variables

--- med/thread.py
+++ med/thread.py
@@ -4,8 +4,6 @@ import time
 
 from .models import *
 
-import schedule
-
 
 def run_continuously(interval=10):
     class CreateNotificationsThread(threading.Thread):
@@ -16,7 +14,7 @@ def run_continuously(interval=10):
                     events = Event.objects.all()
                     for event in events:
                         diff = (event.date_time.timestamp() - datetime.datetime.now(datetime.timezone(offset=datetime.timedelta(hours=3))).timestamp())
-                        if 24 * 3600 - 10 < diff <= 24 * 3600 or 3600 - 10 < diff <= 3600:
+                        if 24 * 3600 - interval < diff <= 24 * 3600 or 3600 - interval < diff <= 3600:
                             for user in event.users.all():
                                 notif = Notification(user=user, text=event.name, name="EventNot", event=event)
                                 notif.save()
